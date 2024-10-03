5651e0cc6f96b4555b6c5659e91a959931abd0bf
juniormartinez1020 <juniormperalta33@gmail.com>
2024-10-02 21:01:52 -0400

    mostrar las fechas con dayjs

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,16 +1,18 @@
 import { Entypo, FontAwesome } from "@expo/vector-icons";
 import { Image, Text, View } from "react-native";
+import dayjs from 'dayjs'
 
 
 export default function EventListItem({ event }) {
     return (
-        <View className='p-5 gap-3'>
+        <View className='p-5 gap-3 border-b-2 border-gray-200 pb-3'>
         <View className='flex-row'>
           <View className='flex-1 gap-2'>
             <Text
               className='text-lg font-semibold uppercase text-amber-700'
             >
-              22 sept,  · 05.30 ET
+             {dayjs(event.datetime).format('ddd, D MMM')}  · 
+             {dayjs(event.datetime).format('h:mm A')}
             </Text>
             <Text
               className='text-xl font-bold'
